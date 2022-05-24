f7734a28112c8f11c51644a7fb6d210a2f6d5578
N-Shar-ma <bablinaneh@gmail.com>
2022-05-25 01:25:54 +0530

    Increase customizability + UI enhancement

--- app.py
+++ app.py
@@ -2,25 +2,37 @@ import streamlit as st
 from  urllib import parse
 from recommender import hybrid_recommend, get_metadata
 
-st.set_page_config(page_title='Customizable Music Recommendation System', page_icon='🎶')
+st.set_page_config(
+    page_title='Customizable Music Recommendation System', 
+    page_icon='🎶',
+    menu_items={
+        'Get Help': None,
+        'Report a bug': "https://github.com/N-Shar-ma/Customizable-Music-Recommendation-System/issues",
+        'About': "### This project was made as part of Microsoft Engage'22!"
+    }
+)
 
 def change_song(index):
     st.session_state['current_song_index'] = index
 
 if 'current_song_index' not in st.session_state:
-    st.session_state['current_song_index'] = 11726
+    st.session_state['current_song_index'] = 1255
 
 st.title('🎶 Customizable Music Recommendation System 🎶')
 
+st.sidebar.title('Options')
+
 option1 = 'Keep up with what\'s trending'
 option2 = 'Discover hidden gems'
-mode = st.sidebar.selectbox('What kind of song recommendations would you like?', (option1, option2))
+mode = st.sidebar.selectbox('Choose your mode of recommendations', (option1, option2))
 if(mode == option1):
     prioritisePopular = True
 else:
     prioritisePopular = False
 
-recommendations_count = st.sidebar.slider('Upto how many of each kind of recommendations would you like? (lesser means more accurate but more means more variety!)', min_value=1, max_value=10)
+recommendations_count = st.sidebar.slider('Upto how many of each kind of recommendations would you like? (lesser means more accurate but more means more variety!)', min_value=1, max_value=10, value=3)
+
+st.sidebar.write('Choose which kinds of recommendations you would like')
 
 current_song_metadata = get_metadata(st.session_state['current_song_index'])
 
@@ -32,9 +44,11 @@ with st.expander('Show lyrics'):
 recommendations = hybrid_recommend(st.session_state['current_song_index'], recommendations_count , prioritisePopular=prioritisePopular)
 
 for recommendation_type, songs in recommendations.items():
+    if not st.sidebar.checkbox(recommendation_type, value=True):
+        continue
     if(len(songs) == 0):
         continue
-    st.write('####', recommendation_type.upper())
+    st.write('####', recommendation_type.title())
     with st.container():
         for song in songs:
             st.write('- ', song['track_name'], ' - ', song['track_artist'])
